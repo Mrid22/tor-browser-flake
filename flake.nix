c108dcb1c50948dcf3fc123961ffebd93eac1a92
Mrid22 <mridulaga@outlook.com>
2025-12-22 13:56:32 +0100

    trying to wrap tor with mkfirefox

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -12,6 +16,6 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     packages.x86_64-linux.default =
-      pkgs.wrapFirefox pkgs.tor-browser-unwrapped {};
+      pkgs.wrapFirefox pkgs.tor-browser {};
   };
 }
