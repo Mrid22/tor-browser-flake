283bb19a3d3e7312df665452fc890f6613072d19
Mrid22 <mridulaga@outlook.com>
2025-12-22 13:07:34 +0100

    typo

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     self,
     nixpkgs,
   }: let
-    pkgs = "nixpkgs.legacyPackages.x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     packages.x86_64-linux.default = pkgs.wrapFirefox pkgs.tor-browser;
   };
