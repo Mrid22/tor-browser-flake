0de48cdc7c80f765c8df75eff279ec5e79d6bf77
Mrid22 <mridulaga@outlook.com>
2025-12-22 18:16:43 +0100

    trying to fix gtk3

--- flake.nix
+++ flake.nix
@@ -16,7 +16,8 @@
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.default =
-      pkgs.wrapFirefox pkgs.tor-browser {};
+    packages.x86_64-linux.default = pkgs.wrapFirefox pkgs.tor-browser {
+      gtk3 = pkgs.gtk3;
+    };
   };
 }
