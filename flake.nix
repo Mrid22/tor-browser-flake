4daf0695a4890b30f35249275c49d454c88a5872
Mrid22 <mridulaga@outlook.com>
2025-12-24 19:45:57 +0100

    typo

--- flake.nix
+++ flake.nix
@@ -16,8 +16,7 @@
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.default = pkgs.wrapFirefox pkgs.tor-browser {
-        stdenv = pkgs.gccstdenv
-    };
+    packages.x86_64-linux.tor-fix = pkgs.tor-browser.overrideAttrs {};
+    packages.x86_64-linux.default = pkgs.wrapFirefox self.packages.x86_64-linux.tor-fix {};
   };
 }
